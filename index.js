1b45d099084b7c3a20d8043581f5be87704d48b9
ap_boot <sahapri@gmail.com>
2023-05-14 19:54:13 -0400

    changed file path for the readme to be generated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
-const filename = 'README.md'
+const filename = './utils/README.md'
 const questions= () =>{
     return inquirer.prompt ([  
             {
